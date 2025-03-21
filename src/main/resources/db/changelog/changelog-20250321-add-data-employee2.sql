ed5766c03376403dbed7c9fc140d42a05924c8d8
Arief Luthfi Aulia <ariefluthfi.aulia@gmail.com>
2025-03-21 07:04:17 +0700

    Add data  employee, project and time record

---
+++ src/main/resources/db/changelog/changelog-20250321-add-data-employee2.sql
@@ -0,0 +1,12 @@
+INSERT INTO employee (id, name)
+VALUES
+(3, 'Alice'),
+(4, 'Bob'),
+(5, 'Charlie'),
+(6, 'David'),
+(7, 'Emma'),
+(8, 'Frank'),
+(9, 'Grace'),
+(10, 'Hannah'),
+(11, 'Isaac'),
+(12, 'Jack');
---
+++ src/main/resources/db/changelog/changelog-20250321-add-data-project.sql
@@ -0,0 +1,12 @@
+INSERT INTO project (id, name)
+VALUES
+(3, 'Sample Project C'),
+(4, 'Sample Project D'),
+(5, 'Sample Project E'),
+(6, 'Sample Project F'),
+(7, 'Sample Project G'),
+(8, 'Sample Project H'),
+(9, 'Sample Project I'),
+(10, 'Sample Project J'),
+(11, 'Sample Project K'),
+(12, 'Sample Project L');
